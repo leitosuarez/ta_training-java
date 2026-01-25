a1433dc9255d8e055fd4399ba9138b828a8b1764
Leonel Suarez <leoneljunio123@gmail.com>
2026-01-25 15:29:00 -0300

    initial repository structure

---
+++ java/com/epam/training/leonel_suarez/fundamental/main_task/Prueba.java
@@ -0,0 +1,7 @@
+package com.epam.training.leonel_suarez.fundamental.main_task;
+
+public class Prueba {
+    public static void main(String[] args) {
+        System.out.println("Hello User!");
+    }
+}
